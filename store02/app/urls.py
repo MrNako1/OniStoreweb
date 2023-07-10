747b969f2218e9dd4a059463bd2149243c49c551
MattySm <139090532+MattySm@users.noreply.github.com>
2023-07-10 01:39:27 -0400

    Add files via upload

--- store02/app/urls.py
+++ store02/app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import home,productos,carrito,ultimouser,iniciosesion,registro,agregar_producto,prueba
+from .views import home,productos,carrito,ultimouser,iniciosesion,registro,agregar_producto,prueba,listar_productos,modificar_producto, eliminar_producto
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('productos/', productos, name='productos'),
-    path('carrito/', carrito, name='carrito'),
-    path('ultimouser/', ultimouser, name='ultimouser'),
-    path('iniciosesion/', iniciosesion, name='iniciosesion'),
-    path('registro/', registro, name='registro'),
-    path('agregar-producto/', agregar_producto, name='agregar_producto'),
-    path('prueba/',prueba, name = 'prueba'),
+    path('', home, name="home"),
+    path('productos/', productos, name="productos"),
+    path('carrito/', carrito, name="carrito"),
+    path('ultimouser/', ultimouser, name="ultimouser"),
+    path('iniciosesion/', iniciosesion, name="iniciosesion"),
+    path('registro/', registro, name="registro"),
+    path('agregar-producto/', agregar_producto, name="agregar_producto"),
+    path('prueba/',prueba, name = "prueba"),
+    path('listar-productos/',listar_productos, name = "listar_productos"),
+    path('modificar-producto/<id>/', modificar_producto, name="modificar_producto"),
+    path('eliminar-producto/<id>/', eliminar_producto, name="eliminar_producto")
 ]
--- store02/app/views.py
+++ store02/app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Productos
 from .forms import ContactoForm, ProductoForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -47,7 +48,7 @@ def agregar_producto(request):
         formulario = ProductoForm(data=request.POST, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            #data["mensaje"] = "guardado correctamente"
+            messages.success(request, "agregado correctamente")
         else:
             data["form"] = formulario
     
@@ -57,3 +58,36 @@ def agregar_producto(request):
 
 def prueba(request):
     return render(request, 'app/prueba.html')
+
+def listar_productos(request):
+    productos = Productos.objects.all()
+
+    data = {
+        'productos' : productos
+        }
+    return render(request, 'app/newproducto/listar.html', data)
+
+def modificar_producto(request, id):
+    producto = get_object_or_404(Productos, id=id)
+
+    data = {
+        'form': ProductoForm(instance=producto)
+    }
+    
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, instance=producto, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "modificado correctamente")
+            return redirect(to="listar_productos")
+        data["form"] = formulario
+
+    return render(request, 'app/newproducto/modificar.html', data)
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Productos, id=id)
+    producto.delete()
+    messages.success(request, "eliminado correctamente")
+
+    return redirect(to="listar_productos") 
+    
